7438d7250b52be276f61cb815e973c922562dfe4
Javier Prz <58307741+jvprz@users.noreply.github.com>
2023-06-08 11:31:47 +0200

    Song interface

---
+++ cinemadle/src/Interfaces/song.interface.ts
@@ -0,0 +1,7 @@
+export interface Song {
+  name: String;
+  artist: String;
+  album: String;
+  duration_ms: number;
+  preview_url: string;
+}
--- cinemadle/src/app/testing/testing.component.ts
+++ cinemadle/src/app/testing/testing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { enviroment } from 'src/enviroments/enviroment';
 
 @Component({
