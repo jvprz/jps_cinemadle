c064ded1e54ed1b1410c89e20bed0185365258df
Javier Prz <58307741+jvprz@users.noreply.github.com>
2023-06-09 11:27:26 +0200

    Boostrap

--- cinemadle/src/app/components/callback/callback.component.ts
+++ cinemadle/src/app/components/callback/callback.component.ts
@@ -10,35 +10,32 @@ const { spotifyClientId, spotifyClientSecret } = credentials;
   templateUrl: './callback.component.html',
   styleUrls: ['./callback.component.css']
 })
-export class CallbackComponent implements OnInit{
+export class CallbackComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
-      this.route.queryParams.subscribe(params => {
-        const authorizationCode = params['code'];
-        const state = params['state'];
+    this.route.queryParams.subscribe(params => {
+      const authorizationCode = params['code'];
+      const state = params['state'];
 
-        // Acces tokken
-        const tokenEndpoint = 'https://accounts.spotify.com/api/token';
-        const requestBody = {
-          grant_type: 'authorization_code',
-          code: authorizationCode,
-          redirect_uri: window.location.origin + '/callback',
-          client_id: spotifyClientId,
-          client_secret: spotifyClientSecret
-        };
+      // Acces token
+      const tokenEndpoint = 'https://accounts.spotify.com/api/token';
+      const requestBody = {
+        grant_type: 'authorization_code',
+        code: authorizationCode,
+        redirect_uri: window.location.origin + '/callback',
+        client_id: spotifyClientId,
+        client_secret: spotifyClientSecret
+      };
 
-        this.http.post(tokenEndpoint, requestBody).subscribe((response: Object) => {
-          if ('access_token' in response) {
-            const accessToken = response['access_token'] as string;
-            console.log(accessToken);
-          }
-          if ('expires_in' in response) {
-            const expiresIn = response['expires_in'];
-            console.log(expiresIn);
-          }
-        });
+      this.http.post(tokenEndpoint, requestBody).subscribe((response: any) => {
+        const accessToken = response.access_token;
+        const expiresIn = response.expires_in;
+        console.log(accessToken);
+        console.log(expiresIn);
       });
+    });
   }
 }
+
