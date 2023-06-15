477a2a7d74946e76e0c220a21a29c4c397a1cbc2
JPrimee <javierpsantos99@gmail.com>
2023-06-15 10:32:25 +0200

    All footer components

--- cinemadle/src/app/app-routing.module.ts
+++ cinemadle/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CallbackComponent } from './components/callback/callback.component';
+import { FaqsComponent } from './components/faqs/faqs.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
+import { TermsComponent } from './components/terms/terms.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'callback', component: CallbackComponent }
+  { path: '', component:LoginComponent },
+  { path: 'callback', component: CallbackComponent },
+  { path: 'faqs', component: FaqsComponent },
+  { path: 'privacy-policy', component: PrivacyComponent },
+  { path: 'terms-of-service', component: TermsComponent}
 ];
 
 @NgModule({
--- cinemadle/src/app/app.module.ts
+++ cinemadle/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { OAuthModule, OAuthStorage } from 'angular-oauth2-oidc';
 import { LoginComponent } from './components/login/login.component';
 import { AuthService } from './auth/auth.service';
 import { FooterComponent } from './components/footer/footer.component';
+import { FaqsComponent } from './components/faqs/faqs.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
+import { TermsComponent } from './components/terms/terms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    FaqsComponent,
+    PrivacyComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule,
